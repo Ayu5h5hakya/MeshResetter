d6478a53f051ee3379aa3c7925d333620d92954c
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2022-08-01 10:11:34 +0545

    add basic logic for resetting selection

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import maya.cmds as cmds
 
 logging.basicConfig()
 logger = logging.getLogger("mesh_resetter")
@@ -6,8 +7,13 @@ logger.setLevel(logging.INFO)
 
 def reset_to_origin():
     selection = cmds.ls(selection=True)
-    selection_short = cmds.ls(selection=True)
 
     if not selection:
         cmds.warning('Nothing selected. Please select at least one object and try again.')
         return
+    
+    try:
+        for obj in selection:
+            cmds.move(0, 0, 0, obj, a=True, rpr=True)  
+    except Exception as e:
+        logger.debug(str(e))
