beb0b6421e1796f98448ee0542aa6cd2beed3132
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2022-08-05 09:44:06 +0545

    add tool result reporting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 import maya.cmds as cmds
+import sys
 
 logging.basicConfig()
 logger = logging.getLogger("mesh_resetter")
@@ -11,6 +12,7 @@ def reset_to_origin():
     if not selection:
         cmds.warning('Nothing selected. Please select at least one object and try again.')
         return
+    counter = 0
     errors = ''
     try:
         for obj in selection:
@@ -21,5 +23,14 @@ def reset_to_origin():
         if errors != '':
             print('ERROR!')
             print(errors)
+
+        if counter > 0:
+            pivot_pos = 'origin'
+            is_plural = 'objects were'
+            affected = str(counter)
+            if counter == 1:
+                is_plural = ' was'
+            message = affected + ' ' + is_plural + ' moved to the ' + pivot_pos
+            sys.stdout.write(message)
     except Exception as e:
         logger.debug(str(e))
